685e8e8ce343264b312f89f495989b1aca4f01ec
akis <akis@ecoc.io>
2020-01-30 13:11:12 +0700

    adding web3,ecoc,web3,dotenv,mocha; checking connections

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const Web3 = require("web3");
+const { Ecocw3 } = require("ecoweb3");
+
+const ETH = {
+  ADDR: process.env.ETH_ADDR,
+  PRIV_KEY: process.env.ETH_PRIV_KEY
+};
+
+const INFURA = {
+  ENDPOINT: process.env.INFURA_ENDPOINT,
+  SECRET: process.env.INFURA_SECRET
+};
+
+const ECOC = {
+  ADDR: process.env.ECOC_ADDR,
+  PRIV_KEY: process.env.ECOC_PRIV_KEY,
+  ENDPOINT: process.env.ECOCNODE_ENDPOINT
+};
+
+const eth_web3 = new Web3(new Web3.providers.HttpProvider(INFURA.ENDPOINT));
+const ecoc_web3 = Ecocw3.Rpc(ECOC.ENDPOINT);
+
+eth_web3.eth.net.isListening().then(results => {
+  console.log("connected to Infura: " + results);
+});
+
+async function isConnected() {
+  return await ecoc_web3.isConnected();
+}
+
+isConnected().then(results => {
+  console.log("connected to ecochain node: " + results);
+});
