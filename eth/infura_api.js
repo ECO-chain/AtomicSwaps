b2802f8e4c5fd4cbde0c238dff9c4b1ea676cce3
akis <akis@ecoc.io>
2020-02-11 13:59:13 +0700

    starting infura api function calls; starting eth swap contract

---
+++ eth/infura_api.js
@@ -0,0 +1,55 @@
+require("dotenv").config({ path: "../.env" });
+const axios = require("axios");
+
+const ETH = {
+  ADDR: process.env.ETH_ADDR,
+  PRIV_KEY: process.env.ETH_PRIV_KEY,
+  NET: process.env.CHAIN_MODE,
+  ENDPOINT: process.env.INFURA_ENDPOINT,
+  SECRET: process.env.INFURA_SECRET,
+  CONTRACT_ADDR: process.env.ETH_SMARTCONTRACT,
+  RECEIVERS_ADDR: process.env.ETH_RECEIVER_ADDR
+};
+if (ETH.NET == "Testnet") {
+  ETH.ENDPOINT = "https://ropsten." + ETH.ENDPOINT;
+} else {
+  ETH.ENDPOINT = "https://" + ETH.ENDPOINT;
+}
+
+const HEADERS = {
+  "Content-Type": "application/json"
+};
+
+async function getBlockHeight(base = "hex") {
+  let config = {
+    headers: HEADERS
+  };
+  let data = {
+    jsonrpc: "2.0",
+    method: "eth_blockNumber",
+    params: [],
+    id: 1
+  };
+  return await axios
+    .post(ETH.ENDPOINT, data, config)
+    .then(response => {
+      if (response.status == 200) {
+        if (base == "decimal") {
+          msg = parseInt(response.data.result, 16);
+        } else {
+          msg = response.data.result;
+        }
+        return msg;
+      } else {
+        msg = "Server returned status " + response.status;
+        return msg;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+module.exports = {
+  infuraCurrentHeight: getBlockHeight
+};
---
+++ eth/swapContract.js
@@ -0,0 +1,227 @@
+require("dotenv").config({ path: "../.env" });
+const utils = require("./utils.js");
+const Web3  = require("web3");
+
+const ETH = {
+  ADDR: process.env.ETH_ADDR,
+  PRIV_KEY: process.env.ETH_PRIV_KEY,
+  NET: process.env.CHAIN_MODE,
+  ENDPOINT: process.env.INFURA_ENDPOINT,
+  SECRET: process.env.INFURA_SECRET,
+  CONTRACT_ADDR: process.env.ETH_SMARTCONTRACT,
+  RECEIVERS_ADDR: process.env.ETH_RECEIVER_ADDR
+};
+if (ETH.NET=='Testnet') {
+ETH.ENDPOINT= 'https://ropsten.'+ETH.ENDPOINT
+} else {
+  ETH.ENDPOINT= 'https://'+ETH.ENDPOINT
+};
+
+const web3 = new Web3(ETH.ENDPOINT);
+
+const contract_abi = [
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_AtomicSwapID",
+        type: "bytes32"
+      },
+      {
+        name: "_receiverAddress",
+        type: "address"
+      },
+      {
+        name: "_SHA3Hash",
+        type: "bytes32"
+      },
+      {
+        name: "_blockNumber",
+        type: "uint256"
+      }
+    ],
+    name: "open",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function"
+  },
+  {
+    constant: false,
+    inputs: [
+      {
+        name: "_AtomicSwapID",
+        type: "bytes32"
+      },
+      {
+        name: "_secretKey",
+        type: "bytes"
+      }
+    ],
+    name: "close",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function"
+  },
+  {
+    constant: true,
+    inputs: [
+      {
+        name: "_AtomicSwapID",
+        type: "bytes32"
+      }
+    ],
+    name: "check",
+    outputs: [
+      {
+        name: "timelock",
+        type: "uint256"
+      },
+      {
+        name: "value",
+        type: "uint256"
+      },
+      {
+        name: "receiverAddress",
+        type: "address"
+      },
+      {
+        name: "SHA3Hash",
+        type: "bytes32"
+      },
+      {
+        name: "secretKey",
+        type: "bytes"
+      }
+    ],
+    payable: false,
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "_AtomicSwapID",
+        type: "bytes32"
+      },
+      {
+        indexed: false,
+        name: "_receiverAddress",
+        type: "address"
+      },
+      {
+        indexed: false,
+        name: "_SHA3Hash",
+        type: "bytes32"
+      }
+    ],
+    name: "Open",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "_AtomicSwapID",
+        type: "bytes32"
+      }
+    ],
+    name: "Expire",
+    type: "event"
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        name: "_AtomicSwapID",
+        type: "bytes32"
+      },
+      {
+        indexed: false,
+        name: "_secretKey",
+        type: "bytes"
+      }
+    ],
+    name: "Close",
+    type: "event"
+  }
+];
+
+const contract = web3.eth.Contract(contract_abi, ETH.CONTRACT_ADDR,);
+
+async function call_check(atomic_swap_ID) {
+  let params = {
+    methodArgs: [atomic_swap_ID],
+    senderAddress: ETH.ADDR
+  };
+  //return await contract.call("check", params);
+  /* create the method and call the contract*/
+}
+
+/*
+function wrap_call_check(atomic_swap_ID) {
+  call_check(atomic_swap_ID)
+    .then(results => {
+      r = {
+        timelock: results.executionResult.formattedOutput.timelock,
+        value: results.executionResult.formattedOutput.value,
+        receiverAddress: results.executionResult.formattedOutput.receiverAddress
+        ),
+        HA3Hash: Buffer.from(results.executionResult.formattedOutput.SHA3Hash,'hex').toString(),
+        secretKey: results.executionResult.formattedOutput.secretKey
+      };
+      console.log(r);
+        return r;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+async function send_open(
+  atomic_swap_ID,
+  receiver_addr,
+  SHA3_hash,
+  block_timelock,
+  eth_amount,
+  gas_limit = 250000,
+  gas_price = 0.0000004
+) {
+  let params = {
+    methodArgs: [atomic_swap_ID, receiver_addr, SHA3_hash, block_timelock],
+    amount: eth_amount,
+    gasLimit: gas_limit,
+    gasPrice: gas_price,
+    senderAddress: ETH.ADDR
+  };
+
+  return await contract.send("open", params);
+}
+
+async function send_close(
+  atomic_swap_ID,
+  secret,
+  gas_limit = 250000,
+  gas_price = 0.0000004
+) {
+  let params = {
+    methodArgs: [atomic_swap_ID, secret],
+    amount: 0 ,
+    gasLimit: gas_limit,
+    gasPrice: gas_price,
+    senderAddress: ETH.ADDR
+  };
+
+  return await contract.send("close", params);
+}
+*/
+module.exports = {
+  eth_open_swap : send_open,
+  eth_close_swap : send_close,
+  eth_check_swap : wrap_call_check,
+};
