9ee679df24d7c9d82b8723f029aeb234d5a9549a
akis <akis@ecoc.io>
2020-02-21 17:35:11 +0700

    adding checkSHA3()

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,4 +1,14 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
-module.exports = {
+function checkSHA3(plaintext, hash) {
+  const h_sha3 = crypto.createHash("sha3-256");
+  let digest = h_sha3.update(plaintext).digest("hex");
+  digest = "0x" + digest;
+  if (digest == hash) {
+    return true;
+  }
+  return false;
 }
+module.exports = {
+  checkSHA3: checkSHA3
+};
