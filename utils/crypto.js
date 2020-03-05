fc1ffb862bde3768cd1701611dfd990a6ce95b18
akis <akis@ecoc.io>
2020-03-05 10:34:01 +0700

    documentation

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,5 +1,12 @@
 const crypto = require("crypto");
 
+/**
+ * Sunchronous function
+ * Checks if the hash of plaintext in SHa3-256 matches the givenhash
+ * @param {string} plaintext - message to be hashed in SHA3-256
+ * @param {string} hash - the hash to be checked
+ *  @returns {boolean} - true if hash is correct, else false
+ */
 function checkSHA3(plaintext, hash) {
   const h_sha3 = crypto.createHash("sha3-256");
   let digest = h_sha3.update(plaintext).digest("hex");
